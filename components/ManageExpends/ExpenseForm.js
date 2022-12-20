fc3d3111fabc81b6e25391b6168fba267a210c91
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-20 18:30:05 +0000

    Validation Added

--- components/ManageExpends/ExpenseForm.js
+++ components/ManageExpends/ExpenseForm.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Alert} from 'react-native';
 import Input from './Input';
 import Button from '../ui/Button';
 import {getFormattedDate} from '../../util/date';
@@ -31,6 +31,15 @@ export default function ExpenseForm({
       date: new Date(inputValues.date),
       description: inputValues.description,
     };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert('Invalid input', 'Please check your input values');
+      return;
+    }
 
     onSubmit(expenseData);
   }

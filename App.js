d229bf4624ae69520dd86f743e4b2cb7e7582512
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-20 11:39:57 +0000

    Configuring Navigation

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import {StatusBar, View, Text, StyleSheet} from 'react-native';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import ManageExpensesScreen from './screens/ManageExpensesScreen';
+import AllExpensesScreen from './screens/AllExpensesScreen';
+import RecentExpensesScreen from './screens/RecentExpensesScreen';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen
+        name="Recent Expenses"
+        component={RecentExpensesScreen}
+      />
+      <BottomTabs.Screen name="All Expenses" component={AllExpensesScreen} />
+    </BottomTabs.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <>
       <StatusBar />
-      <View style={styles.container}>
-        <Text>The Expense Tracker App</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="ExpensesOverview">
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen
+            name="ManageExpenses"
+            component={ManageExpensesScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
